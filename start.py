b95b21113dc163e7b5302f4f25b694eabc4027dc
Andrey-Yakovtsev <ayakovtsev@gmail.com>
2021-01-08 14:38:10 +0300

    Как пройтись по потокам?

--- start.py
+++ start.py
@@ -10,26 +10,6 @@ if __name__ == "__main__":
     human_players_count = 2     # int(input('Введите количество людей: '))
     pc_players_count = 5    # int(input('Введите количество компьютерных соперников: '))
 
-    # humans_cards = []
-    # for i in range(1, (human_players_count + 1)):
-    #     human_player = Player()
-    #     humans_cards.append(human_player.validate_card())
-    #
-    # pprint("Ваша Карточка")
-    # pprint(humans_cards)
-    # plain_human_card = plainify_card(humans_cards)
-    #
-    # pc_cards = []
-    # for i in range(1, (pc_players_count + 1)):
-    #     pc_player = Player()
-    #     pc_cards.append(pc_player.validate_card())
-    #
-    # pprint("Карточка компьютера")
-    # pprint(pc_cards)
-    # plain_pc_card = plainify_card(pc_cards)
-
-
-
     pc_players_threads(pc_players_count)
     human_players_threads(human_players_count)
     print(threading.active_count())
