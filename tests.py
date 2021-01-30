394730940598a6b3f22394da7d545e60357400c6
Andrey-Yakovtsev <ayakovtsev@gmail.com>
2021-01-31 00:03:26 +0300

    готовлю ПайТесты

---
+++ tests.py
@@ -0,0 +1,14 @@
+from logic import plainify_card, get_number_from_bag, Player
+
+
+def test_card_generation():
+    # проверяем, что в карточке 15 номеров, 3 ряда, по 5 номеров больше нуля в каждом
+    pass
+
+def test_card_validation():
+    # проверяем, что в карточке номера не повторяются
+    pass
+
+def test_if_number_in_range():
+    # проверяем, что бочонок в диапазоне от 1 до 90
+    pass
